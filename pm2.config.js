970638e866e9a8ddc53d0fa9547b5abbe6ad72d8
ddhp <sevenknee@gmail.com>
2018-04-16 19:33:17 +0800

    server setting

---
+++ pm2.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+/**
+ * Application configuration section
+ * http://pm2.keymetrics.io/docs/usage/application-declaration/
+ */
+apps : [
+  // First application
+  {
+    name      : 'YunLi 2018',
+    script    : './dist/server/index.js',
+    env: {
+      COMMON_VARIABLE: 'true',
+      NODE_ENV: 'development',
+      DEBUG: '*,-nodemon*,-express*,-send,-babel'
+    },
+    env_stage: {
+      NODE_ENV: 'stage'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    },
+    // "exec_interpreter" : "./node_modules/.bin/babel-node",
+    out_file: 'logs/out.log',
+    err_file: 'logs/err.log'
+  }
+]
+};
