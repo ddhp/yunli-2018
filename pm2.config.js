ab0a34dbe5a93a4ca00ca4d67c4fbe836a0c2e9e
jesse <jesse@linecorp.com>
2020-01-06 19:20:37 +0900

    port 7373

--- pm2.config.js
+++ pm2.config.js
@@ -3,26 +3,26 @@ module.exports = {
  * Application configuration section
  * http://pm2.keymetrics.io/docs/usage/application-declaration/
  */
-apps : [
+  apps: [
   // First application
-  {
-    name      : 'YunLi 2018',
-    script    : './dist/server/index.js',
-    env: {
-      COMMON_VARIABLE: 'true',
-      NODE_ENV: 'development',
-      DEBUG: '*,-nodemon*,-express*,-send,-babel',
-      PORT: '7272'
+    {
+      name: 'YunLi 2018',
+      script: './dist/server/index.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+        NODE_ENV: 'development',
+        DEBUG: '*,-nodemon*,-express*,-send,-babel',
+        PORT: '7373',
+      },
+      env_stage: {
+        NODE_ENV: 'stage',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      // "exec_interpreter" : "./node_modules/.bin/babel-node",
+      out_file: 'logs/out.log',
+      err_file: 'logs/err.log',
     },
-    env_stage: {
-      NODE_ENV: 'stage'
-    },
-    env_production : {
-      NODE_ENV: 'production'
-    },
-    // "exec_interpreter" : "./node_modules/.bin/babel-node",
-    out_file: 'logs/out.log',
-    err_file: 'logs/err.log'
-  }
-]
+  ],
 };
