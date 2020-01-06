a30430715ffeecd9182821499c2bba93a765f11e
jesse <jesse@linecorp.com>
2020-01-06 19:47:01 +0900

    add www

--- certbot.sh
+++ certbot.sh
@@ -4,4 +4,4 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot # 載入 certbot 的 ppa
 sudo apt-get update # 更新 apt-get
 sudo apt-get install python-certbot-nginx # 安裝 python 的 certbot for nginx
-sudo certbot --nginx -d yunli-design.com
+sudo certbot --nginx -d yunli-design.com -d www.yunli-design.com
